1668740545e5736464399b00dba7aab4606d81fc
fcr15 <forrestcreid@yahoo.com>
2024-04-13 21:47:55 -0700

    I cant fix this bug.

--- frontEnd/js/courseSearch.js
+++ frontEnd/js/courseSearch.js
@@ -1,30 +1,40 @@
 const searchEl = document.getElementById("searchInput");
-const divWrapperEl = document.getElementById("divCoursesModal");
+let divWrapperEl = document.getElementById("wrapper");
 let courses;
 let course;
+let divWrapperArray =[];
 
 async function displayCourses() {
   // This needs to be an array from the list of all courses
-  await fetch("http://localhost:3500/course/courseName", {credentials: "include"}).then((result) => {
-    return result.json().then((data)=>{
-        data.splice(0,1);
-        courses=data;
-    })
+  await fetch("http://localhost:3500/course/courseName", {
+    credentials: "include",
   })
-  
-  courses.forEach((element) => {
-    let courseDiv = document.createElement("div");
-    courseDiv.classList.add("modalContent");
-    divWrapperEl.appendChild(courseDiv);
-    let courseButton = document.createElement("button");
-    courseDiv.appendChild(courseButton);
-    element = element.replaceAll("_", " ");
-    element = element.split(" ");
-    element = element[0] + " " + element[1];
-    courseButton.innerHTML = element;
-  });
+    .then((result) => {
+      return result.json().then((data) => {
+        data.splice(0, 1);
+        courses = data;
+      });
+    })
+    .then((_) => {
+      courses.forEach((element) => {
+        let courseDiv = document.createElement("div");
+        courseDiv.classList.add("modalContent");
+        divWrapperEl.appendChild(courseDiv);
+        let courseButton = document.createElement("button");
+        courseDiv.appendChild(courseButton);
+        element = element.replaceAll("_", " ");
+        element = element.split(" ");
+        element = element[0] + " " + element[1];
+        courseButton.innerHTML = element;
+        divWrapperArray.push("0");
+      });
+    });
 }
-const divWrapperArray = Array.from(divWrapperEl.children);
+//divWrapperArray.splice(0, 2);
+//console.log(divWrapperArray.splice(0, 2));
+divWrapperArray[0] = 2;
+divWrapperArray[3] = "3";
+console.log(divWrapperArray);
 
 searchEl.addEventListener("keyup", (event) => {
   const { value } = event.target;
@@ -32,22 +42,10 @@ searchEl.addEventListener("keyup", (event) => {
   const searchQuery = value.toLowerCase();
 
   divWrapperArray.forEach((element) => {
-
     if (element.innerText.substring(searchQuery)) {
-        element.style.display = "block";
-      } else {
-        element.style.display = "none";
-      }
-  })
-/*
-  for (let courseName of divWrapperArray) {
-    console.log(courseName)  
-    let course = courseName
-
-    if (course.includes(searchQuery)) {
-      courseName.style.display = "block";
+      element.style.display = "block";
     } else {
-      courseName.style.display = "none";
+      element.style.display = "none";
     }
-  }*/
+  });
 });
